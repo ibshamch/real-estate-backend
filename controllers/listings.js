48b062c62b3bbbb7102fe5f8a126b9944530db80
Ibsham <progwithibi@gmail.com>
2024-10-11 03:21:12 +0500

    getAllListings function refactored with property details object

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,9 +3,42 @@ const getAllListings = (req, res) => {
     status: "success",
     message: "Listings fetched successfully",
     data: [
-      { id: 1, title: "House 1", price: 250000 },
-      { id: 2, title: "House 2", price: 300000 },
-      { id: 3, title: "House 3", price: 280000 },
+      {
+        title: "Spanish House",
+        property:"House",
+        size: "5 Marla",
+        type: "Sale",
+        price: "$800,000",
+        location: {
+          city: "Lahore",
+          state: "Punjab",
+          country: "Pakistan",
+          location: "DHA",
+          phase: "Phase 6",
+          block: "Block E",
+        },
+        builtIn: 1969,
+        features: {
+          bathrooms: 4,
+          bedrooms: 3,
+          furnished: true,
+          corner: true,
+        },
+        details: "Beatiful Spanish House in Lahore DHA",
+        agentDetails: {
+          name: "Sadaqat Ali",
+          contact: "+923224688760",
+          whatsapp: "+923224688760",
+        },
+        isFeatured: true,
+        imageLinks: {
+          mainImage: "https://example.com/house1.jpg",
+          galleryImages: [
+            "https://example.com/house1-1.jpg",
+            "https://example.com/house1-2.jpg",
+          ],
+        },
+      },
     ],
   });
 };
