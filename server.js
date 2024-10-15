49b1d3c4f4db9fbf13f4e7bd8a633db28c59bf6d
Ibsham Chaudhary <progwithibi@gmail.com>
2024-10-15 11:53:30 +0500

    imported connectDB function in server.js to use it when server starts running

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db")
 const listingRouter = require("./route/listings");
 const testomonialsRouter = require("./route/testomonials");
 const morgan = require("morgan");
+
 // jo is files ma variables hain unko process.env k andr daldo
 dotenv.config({ path: "./config/config.env" });
+
+// Make sure kro is function ko uski dependencies puri mil rhi hain : process.env.Mongo_URI
+connectDB();
 
 const app = express();
 // Ye app ab request accept kr sakti hai or response bhi de skti hai . Ye app variable hamara server hai .
